d3753d391a6fe264a94dae88a24f3ef336db1621
intrnl <intrnl28@gmail.com>
2019-01-19 21:35:31 +0700

    gulpfile: postcss

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,13 @@ const insert = require('gulp-insert')
 const eol = require('gulp-eol')
 
 const stylus = require('gulp-stylus')
-const prefixer = require('gulp-autoprefixer')
 const sourcemap = require('gulp-sourcemaps')
+
+const postcss = require('gulp-postcss')
+const postcssPlugins = [
+  require('autoprefixer')(),
+  require('cssnano')(),
+]
 
 
 const { info: config, main } = require('./src/config.json')
@@ -27,9 +32,9 @@ const current = new Date()
 gulp.task('build:theme', () => gulp.src(`src/${main}`)
   .pipe(sourcemap.init())
   .pipe(stylus({ define: { currentDate: `${current.getDay()}/${current.getMonth()}` } }))
-  .pipe(prefixer())
+  .pipe(postcss(postcssPlugins))
   .pipe(insert.prepend(metaHeader))
-  .pipe(rename((path) => { path.basename = `${config.name}.theme` })) // eslint-disable-line brace-style
+  .pipe(rename((path) => { path.basename = `import` })) // eslint-disable-line brace-style
   .pipe(eol('\r\n', true))
   .pipe(sourcemap.write('.'))
   .pipe(gulp.dest('dist')))
